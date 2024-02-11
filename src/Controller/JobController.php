be632665ca8d4d01bfa08e2a7dce011470630b63
yannhgs <yannhweb@gmail.com>
2024-02-11 15:11:22 +0100

    formulaire "à la main"

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Offre;
 use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,11 +19,24 @@ class JobController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_job_show')]
+    #[Route('/{id}', name: 'app_job_show', requirements: ['id' => '\d+'])]
     public function show(Offre $offre): Response
     {
         return $this->render('job/show.html.twig', [
             'offre' => $offre,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_job_new')]
+    public function new(Request $request): Response
+    {
+        $submittedToken = $request->request->get('token');
+        if ($this->isCsrfTokenValid('test-token', $submittedToken)) {
+
+        }
+        return $this->render('job/new.html.twig', [
+            'nom' => $request->get('Nom'),
+            'description' => $request->get('Description')
         ]);
     }
 
